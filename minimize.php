80247a3905d95f5b0a6a9dc9eedca31e3c8b0c54
Isaac Emmanuel <35623334+1cbyc@users.noreply.github.com>
2023-04-06 18:59:29 +0100

    Update minimize.php

--- minimize.php
+++ minimize.php
@@ -1,7 +1,7 @@
 <?php
 
 // 1. Use memory caching to avoid reading files repeatedly
-$filename = "/path/to/myfile.txt";
+$filename = "/exports/myfile.txt";
 if (isset($cache[$filename])) {
     $content = $cache[$filename];
 } else {
@@ -10,7 +10,7 @@ if (isset($cache[$filename])) {
 }
 
 // 2. Use file locking to prevent multiple processes from writing to the same file simultaneously
-$filename = "/path/to/myfile.txt";
+$filename = "/exports/myfile.txt";
 $fp = fopen($filename, "a");
 if (flock($fp, LOCK_EX)) {
     fwrite($fp, "New data");
@@ -24,13 +24,13 @@ fclose($fp);
 ob_start();
 // your PHP code here
 $content = ob_get_clean();
-file_put_contents("/path/to/myfile.txt", $content, FILE_APPEND);
+file_put_contents("/exports/myfile.txt", $content, FILE_APPEND);
 
 // 4. Use in-memory data structures to avoid writing to files altogether
 $data = array(
-    "name" => "John",
-    "age" => 30,
-    "email" => "john@example.com"
+    "name" => "Babe",
+    "age" => 23,
+    "email" => "babe@theirdaddy.com"
 );
 $json = json_encode($data);
 echo $json;
